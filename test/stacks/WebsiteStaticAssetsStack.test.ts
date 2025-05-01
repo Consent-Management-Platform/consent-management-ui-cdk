b2d197817d64cb07abbda495a024ac04b2482e5b
Mark Sayson <masayson@gmail.com>
2025-04-30 21:21:44 -0700

    Upgrade dependencies

--- test/stacks/WebsiteStaticAssetsStack.test.ts
+++ test/stacks/WebsiteStaticAssetsStack.test.ts
@@ -5,13 +5,33 @@ import { WebsiteFrontendServiceStack } from '../../lib/stacks/WebsiteFrontendSer
 import { WebsiteStaticAssetsStack } from '../../lib/stacks/WebsiteStaticAssetsStack';
 
 describe('WebsiteStaticAssetsStack', () => {
+  // Normalize template attributes that change independently of CDK code changes
+  function cleanDynamicResources(templateJson: any) {
+    const CODE_KEY = 'Code';
+    const CONTENT_KEY = 'Content';
+    const PROPERTIES_KEY = 'Properties';
+    const RESOURCES_KEY = 'Resources';
+    const S3KEY_KEY = 'S3Key';
+
+    // Normalize S3Key values that change independently of CDK code changes
+    for (const resourceKey in templateJson[RESOURCES_KEY]) {
+      if (resourceKey.startsWith('CustomCDKBucketDeployment') &&
+          templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY]) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY][S3KEY_KEY];
+      } else if (resourceKey.startsWith('DeployStaticWebsiteAssetsAwsCli')) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CONTENT_KEY][S3KEY_KEY];
+      }
+    }
+  }
+
   it('creates the expected CloudFormation template from CDK', () => {
     const app = new App();
     const frontendStack = new WebsiteFrontendServiceStack(app, 'WebsiteFrontendServiceStack');
     const staticAssetsStack = new WebsiteStaticAssetsStack(app, 'WebsiteStaticAssetsStack', {
       lambdaRole: frontendStack.lambdaRole,
     });
-    const template = Template.fromStack(staticAssetsStack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const templateJson = Template.fromStack(staticAssetsStack).toJSON();
+    cleanDynamicResources(templateJson);
+    expect(templateJson).toMatchSnapshot();
   });
 });
