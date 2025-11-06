2ef0c5a442f420c0c30d13d9f50fc0dc2a56eab8
Mark Sayson <masayson@gmail.com>
2025-11-06 00:02:20 -0800

    Combine nested CDK stacks into a single stack to simplify deployments durin g initial development

--- lib/stacks/WebsiteStack.ts
+++ lib/stacks/WebsiteStack.ts
@@ -1,21 +1,165 @@
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Duration, Fn, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, LambdaRestApi, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { BehaviorOptions, CachePolicy, Distribution, OriginRequestPolicy, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { HttpOrigin, S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ArnPrincipal, ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Architecture, Code, Function, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
+import * as path from 'path';
 
-import { WebsiteFrontendServiceStack } from './WebsiteFrontendServiceStack';
-import { WebsiteStaticAssetsStack } from './WebsiteStaticAssetsStack';
+import { CFN_OUTPUT_API_GATEWAY_URL, CFN_OUTPUT_CLOUDFRONT_URL } from '../constants/cloudformationOutputs';
+import { WEBSITE_STATIC_ASSETS_BUCKET_NAME_PREFIX } from '../constants/s3';
 
 /**
- * The main stack that defines the website infrastructure.
- *
- * Contains nested child stacks for the frontend service and static assets.
+ * Defines the website infrastructure.
  */
 export class WebsiteStack extends Stack {
+  private readonly lambdaRole: Role;
+  private readonly staticAssetsBucket: Bucket;
+  private readonly cloudFrontDistribution: Distribution;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const frontendServiceStack = new WebsiteFrontendServiceStack(this, 'WebsiteFrontendServiceStack');
-    new WebsiteStaticAssetsStack(this, 'WebsiteStaticAssetsStack', {
-      lambdaRole: frontendServiceStack.lambdaRole,
+    // Lambda function which hosts the web app
+    this.lambdaRole = this.createLambdaRole();
+    const lambdaFunction: Function = this.createLambdaFunction(this.lambdaRole);
+
+    // API Gateway for Lambda integration
+    const api: RestApi = this.createRestApiGateway(lambdaFunction);
+
+    this.createCloudFormationOutputs(api);
+
+    this.staticAssetsBucket= this.createStaticAssetsBucket();
+
+    this.cloudFrontDistribution = this.createCloudFrontDistribution();
+
+    // Deploy static assets from the Next.js app to the S3 bucket
+    new BucketDeployment(this, 'DeployStaticWebsiteAssets', {
+      sources: [Source.asset('../consent-management-ui/.next/static')],
+      destinationBucket: this.staticAssetsBucket,
+      distribution: this.cloudFrontDistribution,
+      memoryLimit: 1024,
     });
+  }
+
+  // Creates IAM role for the frontend Lambda function
+  private createLambdaRole(): Role {
+    const lambdaRole: Role = new Role(this, 'NextJsLambdaRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+      ],
+    });
+    lambdaRole.addToPolicy(new PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`arn:aws:s3:::${WEBSITE_STATIC_ASSETS_BUCKET_NAME_PREFIX}-${this.account}/*`],
+    }));
+    return lambdaRole;
+  }
+
+  // Creates the Lambda function that hosts the web app service code
+  private createLambdaFunction(lambdaRole: Role): Function {
+    // The AWS Lambda Adapter layer is used to run web apps in AWS Lambda.
+    // Ref: https://github.com/awslabs/aws-lambda-web-adapter
+    const lambdaAdapterLayer = LayerVersion.fromLayerVersionArn(
+      this,
+      'LambdaWebAdapterLayer',
+      `arn:aws:lambda:${this.region}:753240598075:layer:LambdaAdapterLayerArm64:25`
+    );
+
+    return new Function(this, 'WebsiteFrontendLambda', {
+      architecture: Architecture.ARM_64,
+      code: Code.fromAsset(path.join(__dirname, '../../../consent-management-ui/.next/standalone')),
+      handler: 'run.sh',
+      memorySize: 1024,
+      role: lambdaRole,
+      runtime: Runtime.NODEJS_22_X,
+      timeout: Duration.seconds(10),
+      // Environment variables used by AWS Lambda Adapter
+      environment: {
+        'AWS_LAMBDA_EXEC_WRAPPER': '/opt/bootstrap',
+        'RUST_LOG': 'info',
+        'PORT': '8080',
+      },
+      layers: [lambdaAdapterLayer],
+    });
+  }
+
+  // Creates API Gateway for the web app
+  private createRestApiGateway(lambdaFunction: Function) {
+    const api: RestApi = new LambdaRestApi(this, 'WebsiteFrontendApiGateway', {
+      handler: lambdaFunction,
+      proxy: false,
+    });
+
+    const apiResource = api.root.addResource('{proxy+}');
+    apiResource.addMethod('ANY', new LambdaIntegration(lambdaFunction));
+    // Handle CORS preflight requests
+    apiResource.addMethod('OPTIONS');
+    return api;
+  }
+
+  // Creates CloudFormation Outputs for other stacks to reference
+  private createCloudFormationOutputs(api: RestApi): void {
+    new CfnOutput(this, 'ApiGatewayURL', {
+      description: 'Consent Management Website API Gateway URL',
+      exportName: CFN_OUTPUT_API_GATEWAY_URL,
+      value: api.url,
+    });
+  }
+
+  private createStaticAssetsBucket(): Bucket {
+    const staticAssetsBucket: Bucket = new Bucket(this, 'ConsentManagementWebsiteStaticAssetsBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
+      bucketName: `${WEBSITE_STATIC_ASSETS_BUCKET_NAME_PREFIX}-${this.account}`,
+      websiteIndexDocument: 'index.html',
+    });
+
+    // Add bucket policy to allow the website's Lambda function to access the bucket
+    staticAssetsBucket.addToResourcePolicy(new PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [staticAssetsBucket.arnForObjects('*')],
+      principals: [new ArnPrincipal(this.lambdaRole.roleArn)],
+    }));
+
+    return staticAssetsBucket;
+  }
+
+  private createCloudFrontDistribution(): Distribution {
+    const staticAssetsBehaviour: BehaviorOptions = {
+      origin: S3BucketOrigin.withOriginAccessControl(this.staticAssetsBucket),
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    };
+
+    const apiUrl = Fn.importValue(CFN_OUTPUT_API_GATEWAY_URL);
+    // Directly using the API Gateway URL results in a
+    // "Invalid request provided: AWS::CloudFront::Distribution: The parameter origin name cannot contain a colon."
+    // error, so we need to extract the domain name from the URL.
+    // Ref: https://stackoverflow.com/a/72010828
+    const parsedApiUrl = Fn.select(2, Fn.split('/', apiUrl));
+
+    const cloudFrontDistribution = new Distribution(this, 'WebsiteCloudFrontDistribution', {
+      defaultBehavior: staticAssetsBehaviour,
+      additionalBehaviors: {
+        '/_next/*': staticAssetsBehaviour,
+        'api/*': {
+          origin: new HttpOrigin(parsedApiUrl),
+          cachePolicy: CachePolicy.CACHING_DISABLED,
+          originRequestPolicy: OriginRequestPolicy.ALL_VIEWER,
+        }
+      },
+    });
+
+    // Output the CloudFront distribution URL to enable use by other stacks
+    new CfnOutput(this, 'CloudFrontURL', {
+      exportName: CFN_OUTPUT_CLOUDFRONT_URL,
+      value: cloudFrontDistribution.distributionDomainName,
+    });
+
+    return cloudFrontDistribution;
   }
 }
--- test/stacks/WebsiteStack.test.ts
+++ test/stacks/WebsiteStack.test.ts
@@ -3,10 +3,40 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { WebsiteStack } from '../../lib/stacks/WebsiteStack';
 
 describe('WebsiteStack', () => {
+  // Normalize template attributes that change independently of CDK code changes
+  function cleanDynamicResources(templateJson: any) {
+    const CODE_KEY = 'Code';
+    const CONTENT_KEY = 'Content';
+    const METADATA_KEY = 'Metadata';
+    const PROPERTIES_KEY = 'Properties';
+    const RESOURCES_KEY = 'Resources';
+    const S3KEY_KEY = 'S3Key';
+    const SOURCE_OBJECT_KEYS_KEY = 'SourceObjectKeys';
+
+    for (const resourceKey in templateJson[RESOURCES_KEY]) {
+      if (resourceKey == 'WebsiteFrontendLambdaB76C5B1A' &&
+          templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY]) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY][S3KEY_KEY];
+      } else if (resourceKey.startsWith('WebsiteFrontendApiGatewayDeployment') &&
+          templateJson[RESOURCES_KEY][resourceKey][METADATA_KEY]) {
+        delete templateJson[RESOURCES_KEY][resourceKey][METADATA_KEY];
+      } else if (resourceKey.startsWith('CustomCDKBucketDeployment') &&
+          templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY]) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY][S3KEY_KEY];
+      } else if (resourceKey.startsWith('DeployStaticWebsiteAssetsAwsCli')) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CONTENT_KEY][S3KEY_KEY];
+      } else if (resourceKey.startsWith('DeployStaticWebsiteAssetsCustomResource') &&
+          templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][SOURCE_OBJECT_KEYS_KEY]) {
+        delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][SOURCE_OBJECT_KEYS_KEY];
+      }
+    }
+  }
+
   it('creates the expected CloudFormation template from CDK', () => {
     const app = new App();
     const stack = new WebsiteStack(app, 'WebsiteStack');
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const templateJson = Template.fromStack(stack).toJSON();
+    cleanDynamicResources(templateJson);
+    expect(templateJson).toMatchSnapshot();
   });
 });
