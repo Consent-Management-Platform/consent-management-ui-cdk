2d69bf020bf228c4340420831efc3791ef060b21
Mark Sayson <masayson@gmail.com>
2025-11-05 22:46:43 -0800

    Upgrade dependencies

--- test/stacks/WebsiteFrontendServiceStack.test.ts
+++ test/stacks/WebsiteFrontendServiceStack.test.ts
@@ -7,6 +7,7 @@ describe('WebsiteFrontendServiceStack', () => {
   // Normalize template attributes that change independently of CDK code changes
   function cleanDynamicResources(templateJson: any) {
     const CODE_KEY = 'Code';
+    const METADATA_KEY = 'Metadata';
     const PROPERTIES_KEY = 'Properties';
     const RESOURCES_KEY = 'Resources';
     const S3KEY_KEY = 'S3Key';
@@ -15,6 +16,8 @@ describe('WebsiteFrontendServiceStack', () => {
       if (resourceKey == 'WebsiteFrontendLambdaB76C5B1A' &&
           templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY]) {
         delete templateJson[RESOURCES_KEY][resourceKey][PROPERTIES_KEY][CODE_KEY][S3KEY_KEY];
+      } else if (resourceKey == 'WebsiteFrontendApiGatewayDeploymentD682883839f1ff07c38736184c0bcb0220e6deec') {
+        delete templateJson[RESOURCES_KEY][resourceKey][METADATA_KEY];
       }
     }
   }
