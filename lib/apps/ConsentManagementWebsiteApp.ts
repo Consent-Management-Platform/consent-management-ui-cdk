63f6dba3f13f59787b3d97d32d54b76a570106b0
Mark Sayson <masayson@gmail.com>
2025-11-07 00:03:26 -0800

    Tag all deployed stack resources with service and stack names
    Specifying CloudFormation tags for stack resources
    makes it easier to track what services/stacks
    any given deployed resources is associated with,
    and also allows tracking of spending by any given
    tag.

--- lib/apps/ConsentManagementWebsiteApp.ts
+++ lib/apps/ConsentManagementWebsiteApp.ts
@@ -2,10 +2,16 @@ import { App } from 'aws-cdk-lib';
 
 import { WebsiteStack } from '../stacks/WebsiteStack';
 
+const CONSENT_DEMO_WEBSITE_SERVICE_NAME = 'consent-demo-website';
+const SERVICE_TAG_NAME = 'service';
+const STACK_TAG_NAME = 'stack';
+
 export class ConsentManagementWebsiteApp extends App {
   constructor() {
     super();
 
-    new WebsiteStack(this, 'ConsentManagementWebsiteStack');
+    const websiteStack = new WebsiteStack(this, 'ConsentManagementWebsiteStack');
+    websiteStack.addStackTag(SERVICE_TAG_NAME, CONSENT_DEMO_WEBSITE_SERVICE_NAME);
+    websiteStack.addStackTag(STACK_TAG_NAME, websiteStack.stackName);
   }
 }
